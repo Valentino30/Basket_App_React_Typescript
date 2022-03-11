9e4f4bf24f44bcc6e3631970db7691769a6d6b5d
Valentino <valentino@secret-source.eu>
2022-03-11 16:01:15 +0100

    Improve remove item icon button styling

--- src/shared/IconButton/Button/styles.tsx
+++ src/shared/IconButton/Button/styles.tsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
 export const StyledIconButton = styled.button`
+  background-color: snow;
+  font-weight: bold;
   cursor: pointer;
+  border: none;
+  color: gray;
 `;
